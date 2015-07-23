cdf789abcfccc4b67d9ba7efd861aa313eb7611b
Lukas <lukealization@gmail.com>
2015-07-23 21:32:47 +1200

    fix bug with launch reordere

--- app/spacexstats/library/LaunchReorderer.php
+++ app/spacexstats/library/LaunchReorderer.php
@@ -54,12 +54,12 @@ class LaunchReorderer {
     }
 
     private function setMissionProperties($index) {
-        if ($this->currentMissionDt->getSpecificity() == (LaunchSpecificity::Precise || LaunchSpecificity::Day)) {
+        if ($this->currentMissionDt->getSpecificity() == LaunchSpecificity::Precise || $this->currentMissionDt->getSpecificity() == LaunchSpecificity::Day) {
             $this->mission->launch_exact = $this->scheduledLaunch;
             $this->mission->launch_approximate = null;
         } else {
-            $this->mission->launch_approx = $this->scheduledLaunch;
-            $this->mission->launch_approximate = null;
+            $this->mission->launch_approximate = $this->scheduledLaunch;
+            $this->mission->launch_exact = null;
         }
         $this->mission->launch_order_id = $index+1;
         $this->mission->launch_specificity = $this->currentMissionDt->getSpecificity();
