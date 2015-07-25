154700e3ee3d69789b934a2d1c37097df7ccd8b3
Lukas <lukealization@gmail.com>
2015-07-26 11:43:13 +1200

    support for optional missions in elasticsearch

--- app/spacexstats/search/SearchProvider.php
+++ app/spacexstats/search/SearchProvider.php
@@ -14,45 +14,54 @@ class SearchProvider {
     }
 
     public function indexObject($object) {
+        $paramBody = [
+            'object_id' => $object->object_id,
+            'user_id' => $object->user_id,
+            'user' => [
+                'user_id' => $object->user->user_id,
+                'username' => $object->user->username
+            ],
+            'mission_id' => $object->mission_id,
+            'type' => $object->type,
+            'subtype' => $object->subtype,
+            'size' => $object->size,
+            'filetype' => $object->filetype,
+            'title' => $object->title,
+            'dimensions' => [
+                'width' => $object->dimension_width,
+                'height' => $object->dimension_height
+            ],
+            'length' => $object->length,
+            'summary' => $object->summary,
+            'author' => $object->author,
+            'attribution' => $object->attribution,
+            'originated_at' => $object->originated_at,
+            'tweet_user_name' => $object->tweet_user_name,
+            'tweet_text' => $object->tweet_text,
+            'status' => $object->status,
+            'visibility' => $object->visibility,
+            'anonymous' => $object->anonymous,
+            'actioned_at' => $object->actioned_at,
+            'tags' => $object->tags()->lists('name')
+        ];
+
+        if ($object->mission()->count() == 1) {
+            $paramBody['mission'] = [
+                'mission_id' => $object->mission->mission_id,
+                'name' => $object->mission->name
+            ];
+        } else {
+            $paramBody['mission'] = [
+                'mission_id' => null,
+                'name' => null
+            ];
+        }
+
         $params = [
             'index' => 'spacexstats',
             'type' => 'objects',
             'id' => $object->object_id,
-
-            'body' => [
-                'object_id' => $object->object_id,
-                'user_id' => $object->user_id,
-                'user' => [
-                    'user_id' => $object->user->user_id,
-                    'username' => $object->user->username
-                ],
-                'mission_id' => $object->mission_id,
-                'mission' => [
-                    'mission_id' => $object->mission->mission_id,
-                    'name' => $object->mission->name
-                ],
-                'type' => $object->type,
-                'subtype' => $object->subtype,
-                'size' => $object->size,
-                'filetype' => $object->filetype,
-                'title' => $object->title,
-                'dimensions' => [
-                    'width' => $object->dimension_width,
-                    'height' => $object->dimension_height
-                ],
-                'length' => $object->length,
-                'summary' => $object->summary,
-                'author' => $object->author,
-                'attribution' => $object->attribution,
-                'originated_at' => $object->originated_at,
-                'tweet_user_name' => $object->tweet_user_name,
-                'tweet_text' => $object->tweet_text,
-                'status' => $object->status,
-                'visibility' => $object->visibility,
-                'anonymous' => $object->anonymous,
-                'actioned_at' => $object->actioned_at,
-                'tags' => $object->tags()->toList()
-            ]
+            'body' => $paramBody
         ];
 
         return $this->elasticSearchClient->index($params);
