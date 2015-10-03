178e90003f65985d56ad5531f11ec5e05a5269b2
Lukas <lukealization@gmail.com>
2015-10-03 22:19:49 +1300

    search by mission filter

--- app/spacexstats/search/Search.php
+++ app/spacexstats/search/Search.php
@@ -86,24 +86,24 @@ class Search {
             $limitTypesToThese = 'objects,collections';
         }
 
-        $query = array(
+        $requestBody = array();
+
+        $requestBody['query'] = array(
             'multi_match' => array(
                 'query'     => $search['searchTerm'],
                 'fields'    => array('title^2', 'summary', 'tweet_text', 'article')
             )
         );
 
-        // Add in mission
+        // Filters
         if ($search['constraints']['mission'] != null) {
-            $query['bool']['must']['term']['mission_id'] = $search['constraints']['mission'];
+            $requestBody['filter']['bool']['must']['term']['mission_id'] = $search['constraints']['mission'];
         }
 
         return $this->elasticSearchClient->search(array(
             'index' => Search::INDEX,
             'type' => $limitTypesToThese,
-            'body' => array(
-                'query' => $query
-            )
+            'body' => $requestBody
         ));
     }
 
