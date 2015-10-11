07df802aed9f412c9baec4e6877f6fe2cca29daa
Lukas <lukealization@gmail.com>
2015-10-11 23:44:20 +1300

    update directory in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,8 @@ gulp.task('browsersync', function(gulpCallback) {
         proxy: "spacexstats.dev"
     }, function callback() {
 
-        gulp.watch('app/resources/assets/css/**/*.scss', ['styles']);
-        gulp.watch('app/resources/assets/javascript/**/*.*', ['scripts']);
+        gulp.watch('resources/assets/css/**/*.scss', ['styles']);
+        gulp.watch('resources/assets/javascript/**/*.*', ['scripts']);
 
         gulpCallback();
     });
@@ -37,23 +37,23 @@ gulp.task('scripts', function() {
     var rename = require('gulp-rename');
 
     // Move angular module definition
-    gulp.src('app/resources/assets/javascript/angular/apps/app.js')
+    gulp.src('resources/assets/javascript/angular/apps/app.js')
         .pipe(gulp.dest('public/js'));
 
     // Move angular stuff
-    gulp.src(['app/resources/assets/javascript/angular/**/*.js', '!app/resources/assets/javascript/angular/apps/app.js'])
+    gulp.src(['resources/assets/javascript/angular/**/*.js', '!resources/assets/javascript/angular/apps/app.js'])
         .pipe(concat('spacexstatsApp.js')).on('error', handleError)
         //.pipe(uglify()).on('error', handleError)
         .pipe(gulp.dest('public/js')).on('error', handleError)
         .pipe(browserSync.stream());
 
     // Move templates
-    gulp.src('app/resources/assets/javascript/angular/**/*.html')
+    gulp.src('resources/assets/javascript/angular/**/*.html')
         .pipe(rename({ dirname: ''}))
         .pipe(gulp.dest('public/js/templates'));
 
     // Move library
-    gulp.src('app/resources/assets/javascript/lib/**/*.js')
+    gulp.src('resources/assets/javascript/lib/**/*.js')
         .pipe(gulp.dest('public/js'));
 });
 
@@ -62,7 +62,7 @@ gulp.task('styles', function() {
     var autoprefixer = require('gulp-autoprefixer');
     var sass = require('gulp-sass');
 
-    gulp.src('app/resources/assets/javascript/css/styles.scss')
+    gulp.src('resources/assets/javascript/css/styles.scss')
         .pipe(sass()).on('error', handleError)
         .pipe(autoprefixer())
         .pipe(gulp.dest('public/css'))
@@ -74,21 +74,21 @@ gulp.task('styles', function() {
 gulp.task('images', function() {
     var imagemin = require('gulp-imagemin');
 
-    gulp.src('app/resources/assets/javascript/images/**/*.{jpg,jpeg,png}')
+    gulp.src('resources/assets/javascript/images/**/*.{jpg,jpeg,png}')
         .pipe(imagemin())
         .pipe(gulp.dest('public/images'));
 });
 
 // Fonts Task.
 gulp.task('fonts', function() {
-   gulp.src('app/resources/assets/javascript/fonts/*')
+   gulp.src('resources/assets/javascript/fonts/*')
        .pipe(gulp.dest('public/fonts'));
 });
 
 // Watch task. Watch for changes automatically and recompile the SASS.
 gulp.task('watch', function() {
-    gulp.watch('app/resources/assets/css/**/*.scss', ['styles']);
-    gulp.watch('app/resources/assets/javascript/**/*.*', ['scripts']);
+    gulp.watch('resources/assets/css/**/*.scss', ['styles']);
+    gulp.watch('resources/assets/javascript/**/*.*', ['scripts']);
 });
 
 gulp.task('default', ['styles', 'watch', 'browsersync']);
