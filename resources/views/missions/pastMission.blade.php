d3a1cdb04a16ea2a280fab7c8eb3f887dab869bf
Luke <lukealization@gmail.com>
2015-11-20 12:48:25 +1300

    edge case for no public image

--- resources/views/missions/pastMission.blade.php
+++ resources/views/missions/pastMission.blade.php
@@ -164,12 +164,14 @@
                             {{ $images->count() - 20 }} more...
                         </div>
                     @endif
+                @else
+                    <p class="exclaim">No public images</p>
                 @endif
             </section>
 
             <h2>Videos</h2>
             <section id="videos" class="scrollto container">
-                @if ($mission->launch_video != null)
+                @if ($mission->launch_video)
                     <div class="gr-8">
                         <h3>Launch Video</h3>
                     </div>
