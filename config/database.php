ac7e4210115c1585ddb6b60bb11e267cdbbf3831
Luke <lukealization@gmail.com>
2015-12-04 12:40:58 +1300

    remove quotes of death and update db charset

--- config/database.php
+++ config/database.php
@@ -52,10 +52,10 @@ return [
             'database'  => env('DB_DATABASE'),
             'username'  => env('DB_USERNAME'),
             'password'  => env('DB_PASSWORD'),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix'    => '',
-            'strict'    => false,
+            'strict'    => true,
         ],
 
         'pgsql' => [
