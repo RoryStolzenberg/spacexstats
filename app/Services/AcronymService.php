09c5958c1c424ca1463dc439255be6b29eb87cf6
Luke <lukealization@gmail.com>
2015-12-07 08:34:31 +1300

    added acronyms

--- app/Services/AcronymService.php
+++ app/Services/AcronymService.php
@@ -5,7 +5,35 @@ class AcronymService
 {
     // todo: add relevant acronyms from http://www.decronym.xyz/acronyms/SpaceX.json
     protected $acronyms = [
-        'AGL' => 'Above Ground Level'
+        'ACS'   => 'Attitude Control System',
+        'AGL'   => 'Above Ground Level',
+        'AOS'   => 'Acquisition of Signal',
+        'ASDS'  => 'Autonomous Spaceport Drone Ship',
+        'AVI'   => 'Avionics Operator',
+        'FTS'   => 'Flight Termination System',
+        'GNC'   => 'Guidance, Navigation, & Control',
+        'GTO'   => 'Geostationary Transfer Orbit',
+        'JRTI'  => 'Just Read The Instructions',
+        'LEO'   => 'Low Earth Orbit',
+        'LES'   => 'Launch Escape System',
+        'LOS'   => 'Loss of Signal',
+        'LOX'   => 'Liquid Oxygen',
+        'MECO'  => 'Main Engine Cutoff',
+        'MEO'   => 'Mid Earth Orbit',
+        'Max-Q' => 'Maximum Aerodynamic Pressure',
+        'NET'   => 'No Earlier Than',
+        'OCISLY'=> 'Of Course I Still Love You',
+        'OSM'   => 'Operations Safety Manager',
+        'PU'    => 'Propellant Utilization',
+        'RCO'   => 'Range Control Officer',
+        'RCS'   => 'Reaction Control System',
+        'ROC'   => 'Range Operations Coordinator',
+        'RP-1'  => 'Rocket Propellant 1',
+        'RTLS'  => 'Return To Launch Site',
+        'SECO'  => 'Second stage engine cutoff',
+        'T/E'   => 'Transporter/Erector',
+        'TDRSS' => 'Tracking Data and Relay Satellite System',
+        'TVC'   => 'Thrust Vector Control'
     ];
 
     public function parseAndExpand($input, $replace = false) {
