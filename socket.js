c19826b9b33360db1449b30206a607417cc9f50a
Lukas <lukealization@gmail.com>
2015-12-08 22:47:29 +1300

    more socket bugs

--- socket.js
+++ socket.js
@@ -1,10 +1,17 @@
-var app = require('express')();
-var cors = require('cors');
-app.use(cors());
+//var app = require('express')();
+//var cors = require('cors');
+//app.use(cors());
 var https = require('https');
 var fs = require('fs');
 
-var io = require('socket.io')(https);
+var options = {
+    key: fs.readFileSync('/etc/nginx/ssl/spacexstatsbeta.com/17710/server.key'),
+    cert: fs.readFileSync('/etc/nginx/ssl/spacexstatsbeta.com/17710/server.crt')
+};
+
+var app = https.createServer(options);
+
+var io = require('socket.io')(app);
 
 var redis = require('ioredis');
 var Redis = new redis();
@@ -17,11 +24,6 @@ Redis.on('message', function(channel, message) {
     io.emit(channel + ':' + message.event, message.data);
 });
 
-var options = {
-    key: fs.readFileSync('/etc/nginx/ssl/spacexstatsbeta.com/17710/server.key'),
-    cert: fs.readFileSync('/etc/nginx/ssl/spacexstatsbeta.com/17710/server.crt')
-};
-
-https.createServer(options, app).listen(3000, function() {
+app.listen(3000, function() {
     console.log('listening');
 });
