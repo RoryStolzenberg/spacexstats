1d51fe14b99f2a43d2f24a5e43da9c1940a27828
Luke <lukealization@gmail.com>
2015-12-10 15:17:05 +1300

    make contact links underlined

--- resources/views/about/contact.blade.php
+++ resources/views/about/contact.blade.php
@@ -13,16 +13,16 @@
             <p>If you have a query or tip, please contact us directly using one of the emails below:</p>
 
             <h3>General contact & feedback</h3>
-            <p>For issues relating to your account including billing, general feedback, and enquiries:</p>
-            <a href="mailto:contact@spacexstats.com">contact@spacexstats.com</a>
+            <p>For issues relating to your account including billing, general feedback, and enquiries:<br/>
+            <a href="mailto:contact@spacexstats.com">contact@spacexstats.com</a></p>
 
             <h3>Tips & Information</h3>
-            <p>If you have a tipoff or want to provide information, contact us here. All tips & sources are treated securely.</p>
-            <a href="mailto:info@spacexstats.com">info@spacexstats.com</a>
+            <p>If you have a tipoff or want to provide information, contact us here. All tips & sources are treated securely.<br/>
+            <a href="mailto:info@spacexstats.com">info@spacexstats.com</a></p>
 
             <h3>Website Bugs, Issues, and Security</h3>
-            <p>For issues relating to SpaceXStats' functionality and operation, if you find or a bug, error, or security vulnerability, please contact:</p>
-            <a href="mailto:webmaster@spacexstats.com">webmaster@spacexstats.com</a>
+            <p>For issues relating to SpaceXStats' functionality and operation, if you find or a bug, error, or security vulnerability, please contact:<br/>
+            <a href="mailto:webmaster@spacexstats.com">webmaster@spacexstats.com</a></p>
 
         </main>
     </div>
