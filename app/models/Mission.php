5751818c71a60eff9e9194a8cde13d8864922b81
Luke <lukealization@gmail.com>
2015-12-16 13:00:42 +1300

    fix payload mass ranking attribute

--- app/models/Mission.php
+++ app/models/Mission.php
@@ -271,9 +271,7 @@ class Mission extends Model {
         $missionsOrderedByPayloadMass = DB::table('missions')
             ->select('missions.mission_id')
             ->join('payloads', 'missions.mission_id', '=', 'payloads.mission_id')
-            ->groupBy('missions.mission_id')->orderBy('payloads.mass', 'DESC')->toSql();
-
-        return $missionsOrderedByPayloadMass;
+            ->groupBy('missions.mission_id')->orderByRaw('SUM(payloads.mass) DESC')->get();
 
         foreach ($missionsOrderedByPayloadMass as $ranking => $mission) {
             if ($this->mission_id == $mission->mission_id) {
