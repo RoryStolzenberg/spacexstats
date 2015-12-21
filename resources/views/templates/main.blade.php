5d5e4471da06494c82394ce502796e6bc526a2e5
Lukas <lukealization@gmail.com>
2015-12-22 11:09:02 +1300

    make live links open in a new tab

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -87,6 +87,10 @@
     <link href='https://fonts.googleapis.com/css?family=Source+Code+Pro' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 
+    @if(request()->is('live'))
+        <base target="_blank">
+    @endif
+
     @if (\App::environment('production'))
         <script>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
